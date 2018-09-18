94f00030272fe1a413f905039a3797c5a98c0f02
Denis Carriere <carriere.denis@gmail.com>
2018-09-18 16:25:37 -0400

    add types

---
+++ types/eosforumdapp/vote.d.ts
@@ -0,0 +1,6 @@
+export interface Vote {
+    voter:         string;
+    proposal_name: string;
+    vote:          number;
+    vote_json:     string;
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,5 @@
 export * from "./state";
-export * from "./eosforumdapp";
+export * from "./eosforumrcpp";
 export * from "./eosio/blockInfo";
 export * from "./eosio/chain/get_account";
 export * from "./eosio/chain/get_table_rows";
